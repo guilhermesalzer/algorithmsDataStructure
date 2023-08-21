2ab587e1c0c46bce3e83f9dbd4c5d6d4ab024c26
Guilherme Salzer <guilherme.salzer@gmail.com>
2023-08-21 10:26:14 -0300

    Removing fibonacci

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -2,6 +2,8 @@ import math
 # Sorting algorithms
 
 # Insertion SORT
+
+
 def insertion_sort(A, n):
     for i in range(1, n):
         key = A[i]
@@ -12,12 +14,15 @@ def insertion_sort(A, n):
         A[j+1] = key
     return A
 
+
 B = [31, 41, 59, 26, 41, 58]
 n = len(B)
 
 print('Insertion sort: ', insertion_sort(B, n))
 
 # Merge Sort
+
+
 def merge(A, p, split, r):
     n_l = split - p + 1
     n_r = r - split
@@ -29,7 +34,7 @@ def merge(A, p, split, r):
         L.append(A[p + i])
     for j in range(0, n_r):
         R.append(A[split + j + 1])
-    
+
     i = 0
     j = 0
     k = p
@@ -54,6 +59,7 @@ def merge(A, p, split, r):
         j = j + 1
         k = k + 1
 
+
 def merge_sort(A, p, r):
     if p < r:
         split = math.floor((p+r)/2)
@@ -65,15 +71,6 @@ def merge_sort(A, p, r):
 
     return A
 
+
 # Result
 print('Merge sort: ', merge_sort(B, 0, n-1))
-
-
-# Fibonacci
-def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
-
-print(fibonacci(10))
